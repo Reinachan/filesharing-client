9e4ce0780a4934f7594aa352894ca5490173c07c
mochi <git@daifuku.moe>
2025-03-24 18:32:17 +0000

    add NavBar styling make unordered list inside nav for accessibility

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,42 +3,56 @@ import { NavLink } from "react-router";
 
 import { SessionContext } from "contexts";
 
+import style from "./NavBar.module.css";
+
 export default function NavBar() {
   const { user } = use(SessionContext);
 
   return (
-    <nav>
-      <NavLink to="/download" end>
-        Download
-      </NavLink>
+    <nav className={style.NavBar}>
+      <ul>
+        <li>
+          <NavLink to="/download" end>
+            Download
+          </NavLink>
+        </li>
 
-      {user?.user.permissions.upload_files && (
-        <NavLink to="/upload" end>
-          Upload
-        </NavLink>
-      )}
+        {user?.user.permissions.upload_files && (
+          <li>
+            <NavLink to="/upload" end>
+              Upload
+            </NavLink>
+          </li>
+        )}
 
-      {user?.user.permissions.list_files && (
-        <NavLink to="/files" end>
-          Files
-        </NavLink>
-      )}
+        {user?.user.permissions.list_files && (
+          <li>
+            <NavLink to="/files" end>
+              Files
+            </NavLink>
+          </li>
+        )}
 
-      {user?.user.permissions.manage_users && (
-        <NavLink to="/users" end>
-          Users
-        </NavLink>
-      )}
+        {user?.user.permissions.manage_users && (
+          <li>
+            <NavLink to="/users" end>
+              Users
+            </NavLink>
+          </li>
+        )}
 
-      {user ? (
-        <NavLink to="/profile" end>
-          {user.user.username}
-        </NavLink>
-      ) : (
-        <NavLink to="/signin" end>
-          Sign in
-        </NavLink>
-      )}
+        <li>
+          {user ? (
+            <NavLink to="/profile" end>
+              {user.user.username}
+            </NavLink>
+          ) : (
+            <NavLink to="/signin" end>
+              Sign in
+            </NavLink>
+          )}
+        </li>
+      </ul>
     </nav>
   );
 }
