c3dd9bc7d6406e8568485d9ed1a81698f267ac18
mochi <git@daifuku.moe>
2025-03-28 00:31:04 +0000

    add Button documentation

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -17,6 +17,14 @@ interface RegularButton {
 type ButtonProps = (RegularButton | DeleteButton) &
   ButtonHTMLAttributes<HTMLButtonElement>;
 
+/**
+ * Creates a button
+ * @param kind - The kind of the button (default is "secondary")
+ * @param children - Text that goes inside the button (should not be used if kind="delete")
+ * @param ...args - Any other button properties
+ * @returns A button of the specified kind
+ * @example <Button kind="primary">Button</Button>
+ */
 export default function Button({
   kind = "secondary",
   children,
