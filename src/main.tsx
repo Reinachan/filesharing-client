0f5643c9963cca6c9ff631cb0073746965ef41f5
Reinachan <github@reinina.moe>
2025-03-28 01:32:21 +0100

    load css in html instead of js prevents flash of unstyled content

--- src/main.tsx
+++ src/main.tsx
@@ -6,8 +6,6 @@ import { SessionContextProvider } from "contexts";
 
 import { Router } from "./Router";
 
-import "styles/index.css";
-
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
